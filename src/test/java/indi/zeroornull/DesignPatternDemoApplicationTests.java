53967f5022935d8f0ca79094e5484e344b71a53b
zeroornull <xxpfantastic@yahoo.com>
2024-03-29 20:41:04 +0800

    init commit.

---
+++ src/test/java/indi/zeroornull/DesignPatternDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package indi.zeroornull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternDemoApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
